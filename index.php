b99df326b8c7fb990898f584b60723a33ecc4002
I481695 <mette.ghijsen@student.fontys.nl>
2021-11-18 15:17:38 +0100

    gradient geprogrammeerd door gebruik granim.js

--- index.php
+++ index.php
@@ -5,9 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <script src="granim.min.js"></script>
+    <title>Granim.js Demo</title>
 </head>
 <body>
-
+<canvas id="canvas-basic">
+</canvas>
+<script>
+    let granimInstance = new Granim({
+        element: '#canvas-basic',
+        direction: 'left-right',
+        isPausedWhenNotInView: false,
+        states : {
+            "default-state": {
+                gradients: [
+                    ['#b200ff', '#1100ff'],
+                    ['#26ff00', '#ff9c00'],
+                    ['#ff0000', '#fc00ff']
+                ]
+            }
+        }
+    });
+</script>
 </body>
 </html>
